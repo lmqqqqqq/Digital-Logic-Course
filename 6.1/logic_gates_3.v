b35355d219114ff3c36c2d0aa45e3c70fd0d9a17
Eric Lu <2940436026@qq.com>
2022-04-28 21:17:04 +0800

    upload 6.1

---
+++ 6.1/logic_gates_3.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/09/28 16:12:06
+// Design Name: 
+// Module Name: logic_gates_3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module logic_gates_3( iA, iB,oAnd,oOr,oNot );
+   input iA,iB;
+   output reg oAnd,oOr,oNot;
+   always @ (*)
+   begin
+      oAnd = iA & iB;
+      oOr = iA | iB;
+      oNot = ~iA;
+   end
+endmodule
